6602358f6a566ac817fe001634467009fe360f87
Javier Oswaldo <oswaldoj157@gmail.com>
2024-07-04 09:40:09 -0500

    Conexion udp tcp juntos

--- serv.cpp
+++ serv.cpp
@@ -9,6 +9,13 @@
 #include <sys/socket.h>
 #include <fstream>
 #include <random>
+#include <sys/types.h>
+#include <netinet/in.h>
+using namespace std;
+#define UDP_PORT 9090
+
+std::map<std::string, sockaddr_in> udp_clients;
+std::map<std::string, sockaddr_in> neural_clients;
 
 constexpr int PORT = 8080;
 constexpr int BUFFER_SIZE = 150;
@@ -159,35 +166,94 @@ void manejar_cliente(int socket_cliente, sockaddr_in direccion_cliente) {
     }
 }
 
+void udp_send_function(int udp_socket, const char *message, const std::map<std::string, sockaddr_in> &clients) {
+    for (const auto &client : clients) {
+        std::cout << "Attempting to send message to " << inet_ntoa(client.second.sin_addr) << ":" << ntohs(client.second.sin_port) << std::endl;
+        if (sendto(udp_socket, message, strlen(message), 0, (struct sockaddr *)&client.second, sizeof(client.second)) < 0) {
+            std::cerr << "Error sending message to " << inet_ntoa(client.second.sin_addr) << std::endl;
+        } else {
+            std::cout << "Message sent to " << inet_ntoa(client.second.sin_addr) << ":" << ntohs(client.second.sin_port) << std::endl;
+        }
+    }
+}
+
+void udp_receive_function(int udp_socket) {
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    char buffer[1024];
+
+    while (true) {
+        int bytes_received = recvfrom(udp_socket, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (bytes_received > 0) {
+            buffer[bytes_received] = '\0';
+            std::string client_ip = inet_ntoa(client_addr.sin_addr);
+
+            std::cout << "Received message from " << client_ip << ":" << ntohs(client_addr.sin_port) << " - " << buffer << std::endl;
+
+            if (buffer[0] == '1') {
+                neural_clients[client_ip] = client_addr;
+                std::cout << "Stored neural client " << client_ip << ":" << ntohs(client_addr.sin_port) << std::endl;
+            } else if (buffer[0] == '2') {
+                udp_clients[client_ip] = client_addr;
+                std::cout << "Stored UDP client " << client_ip << ":" << ntohs(client_addr.sin_port) << std::endl;
+            }
+
+            // Enviar el mensaje a todos los clientes en neural_clients si el mensaje comienza con 'A'
+            if (buffer[1] == 'A') {
+                std::cout << "Message enviado a redN: " << buffer << std::endl;
+                udp_send_function(udp_socket, buffer , neural_clients);
+            } else {
+                // Imprimir el mensaje si no cumple ninguna condición
+                std::cout << "Message no entro Condicion: " << buffer << std::endl;
+            }
+        }
+    }
+}
+
 int main() {
     int socket_servidor, socket_cliente;
     struct sockaddr_in direccion_servidor, direccion_cliente;
     socklen_t len_direccion = sizeof(direccion_cliente);
 
-    // Crear socket
+    int udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    // Crear socket TCP
     if ((socket_servidor = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         std::cerr << "Error al crear el socket" << std::endl;
         return -1;
     }
 
-    // Configurar dirección del servidor
+    // Configurar dirección del servidor TCP
     direccion_servidor.sin_family = AF_INET;
     direccion_servidor.sin_addr.s_addr = INADDR_ANY;
     direccion_servidor.sin_port = htons(PORT);
 
-    // Enlazar el socket a la dirección y puerto
+    // Enlazar el socket TCP a la dirección y puerto
     if (bind(socket_servidor, (struct sockaddr *)&direccion_servidor, sizeof(direccion_servidor)) < 0) {
         std::cerr << "Error al enlazar el socket" << std::endl;
         close(socket_servidor);
         return -1;
     }
 
-    // Escuchar conexiones entrantes
+    // Escuchar conexiones entrantes TCP
     if (listen(socket_servidor, 3) < 0) {
         std::cerr << "Error al escuchar en el socket" << std::endl;
         close(socket_servidor);
         return -1;
     }
+
+    // Configurar y enlazar socket UDP
+    direccion_servidor.sin_family = AF_INET;
+    direccion_servidor.sin_addr.s_addr = INADDR_ANY;
+    direccion_servidor.sin_port = htons(PORT);
+    if (bind(udp_socket, (struct sockaddr *)&direccion_servidor, sizeof(direccion_servidor)) < 0) {
+        std::cerr << "Error al enlazar el socket UDP" << std::endl;
+        close(udp_socket);
+        return -1;
+    }
+
+    // Iniciar el hilo para recibir mensajes UDP
+    std::thread udp_receive_thread(udp_receive_function, udp_socket);
 
     std::cout << "Servidor escuchando en el puerto " << PORT << std::endl;
 
@@ -200,10 +266,12 @@ int main() {
 
         std::cout << "Nueva conexión aceptada" << std::endl;
 
-        // Crear un hilo para manejar al cliente
+        // Crear un hilo para manejar al cliente TCP
         std::thread(manejar_cliente, socket_cliente, direccion_cliente).detach();
     }
 
+    udp_receive_thread.join();
     close(socket_servidor);
+    close(udp_socket);
     return 0;
 }
